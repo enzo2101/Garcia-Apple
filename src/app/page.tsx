8524190c405c10c08216a0aa6760cd830a8b5da1
Enzo Ragazzoni <enzoragazzoni21@gmail.com>
2024-03-14 15:31:00 -0300

    feat(Design): import logo and adjustments on header and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Header from "~/components/Header/Header";
 
 export default function HomePage() {
   return (
-    <div >
+    <div>
       <Header />
-      <div className="h-screen">
-        <div className="bg-black h-3/5">
-          <div className="flex align-baseline justify-between px-24 pt-11">
-            <div className="space-y-14 w-3/5 ">
+      <div className="h-screen w-full flex justify-center">
+        <div className="bg-black h-3/5 w-full flex justify-center ">
+          <div className="flex align-baseline justify-between space-x-1 max-w-[1200px]">
+            <div className="space-y-14 w-3/5 pt-10 ">
               <h1 className="font-bold text-6xl text-white">O Melhor Iphone na sua mão<span className="text-yellow-500">.</span></h1>
               <p className="text-gray-400">Nós da Garcia Apple trabalhamos os iphones
                 com melhor estado de conservação de Natal-RN</p>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,25 +3,27 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div className="bg-black w-full p-4 flex justify-between items-center">
-      <div>
-        <h1 className="text-white">Logo</h1>
-      </div>
-      <div className="md:hidden">
-        <select className="bg-black border-none text-white" onChange={(e) => window.location.href = e.target.value}>
-          <option value="">☰</option>
-          <option value="/parcela">Parcela</option>
-          <option value="/produtos">Produtos</option>
-          <option value="/contatos">Contatos</option>
-          <option value="/recibos">Recibos</option>
-        </select>
-      </div>
+    <div className="bg-black w-full">
+      <div className="text-xl p-10 flex max-w-[1200px] justify-between ml-auto mr-auto">
+        <div>
+          <img src="./Logo.png" alt="" />
+        </div>
+        <div className="md:hidden">
+          <select className="bg-black border-none text-white" onChange={(e) => window.location.href = e.target.value}>
+            <option value="">☰</option>
+            <option value="/parcela">Parcela</option>
+            <option value="/produtos">Produtos</option>
+            <option value="/contatos">Contatos</option>
+            <option value="/recibos">Recibos</option>
+          </select>
+        </div>
 
-      <div className="hidden md:flex items-center space-x-5">
-        <Link className="text-white" href={"/parcela"}>Parcela</Link>
-        <Link className="text-white" href={"/produtos"}>Produtos</Link>
-        <Link className="text-white" href={"/contatos"}>Contatos</Link>
-        <Link className="text-white" href={"/recibos"}>Recibos</Link>
+        <div className="hidden md:flex items-center space-x-5">
+          <Link className="text-white" href={"/parcela"}>Parcela</Link>
+          <Link className="text-white" href={"/produtos"}>Produtos</Link>
+          <Link className="text-white" href={"/contatos"}>Contatos</Link>
+          <Link className="text-white" href={"/recibos"}>Recibos</Link>
+        </div>
       </div>
     </div>
   );
