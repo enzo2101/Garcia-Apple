ceb51134c60de203f44fef5a373130f98fd05ade
Enzo Ragazzoni <enzoragazzoni21@gmail.com>
2024-03-15 16:11:47 -0300

    edit home page to mobile first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ export default function HomePage() {
   return (
     <div>
       <Header />
-      <div className="h-screen w-full flex justify-center">
-        <div className="bg-black h-3/5 w-full flex justify-center ">
-          <div className="md:flex lg:align-baseline justify-between space-x-1 max-w-[1200px]">
-            <div className="space-y-14 w-3/5 pt-10 ">
+      <div className="w-full flex justify-center">
+        <div className="bg-black w-full flex justify-center ">
+          <div className="xl:flex align-baseline justify-between space-x-1 xl:max-w-[1200px]">
+            <div className="space-y-14 w-3/5 pt-10 text-center ">
               <h1 className="font-bold text-6xl text-white">O Melhor Iphone na sua mão<span className="text-yellow-500">.</span></h1>
               <p className="text-gray-400">Nós da Garcia Apple trabalhamos os iphones
                 com melhor estado de conservação de Natal-RN</p>
