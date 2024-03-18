3551dcbb44f6147950cb6dfc1326afe4c300ad69
Enzo Ragazzoni <enzoragazzoni21@gmail.com>
2024-03-18 18:40:11 -0300

    refactor logic of store previous state

--- src/app/parcela/page.tsx
+++ src/app/parcela/page.tsx
@@ -1,42 +1,37 @@
 "use client"
 
-import { useState, ChangeEvent, useRef, useEffect } from "react";
-import Header from "~/components/Header/Header";
-import formatCurrency from "~/components/formatCurrency/formatCurrency";
-import { parcelCalculator } from "~/components/parcelCalculator/parcelCalculator";
+import { useState, ChangeEvent, useRef } from "react"; // Importação de hooks e componentes necessários
+import Header from "~/components/Header/Header"; // Importação do componente de cabeçalho
+import formatCurrency from "~/components/formatCurrency/formatCurrency"; // Importação da função de formatação de moeda
+import { parcelCalculator } from "~/components/parcelCalculator/parcelCalculator"; // Importação da função de cálculo de parcela
 
 const Parcela = () => {
-  const [parcel, setParcel] = useState("");
-  const [value, setValue] = useState("");
-  const [downValue, setDownValue] = useState("");
+  // Definição de estados e referências
+  const [parcel, setParcel] = useState(""); // Estado para a quantidade de parcelas
+  const [value, setValue] = useState(""); // Estado para o valor a ser parcelado
+  const [downValue, setDownValue] = useState(""); // Estado para o valor da entrada
 
-  const prevDownValue = useRef("");
-  const prevValue = useRef("");
-  const prevParcel = useRef("");
+  const prevDownValue = useRef(""); // Referência para o valor da entrada anterior
+  const prevValue = useRef(""); // Referência para o valor a ser parcelado anterior
+  const prevParcel = useRef(""); // Referência para a quantidade de parcelas anterior
 
-/*   const [informedDownValue, setInformedDownValue] = useState("");
-  const [informedValue, setInformedValue] = useState("");
-  const [informedParcel, setInformedParcel] = useState(""); */
+  const [parcelValue, setParcelValue] = useState(""); // Estado para o valor de cada parcela
 
-  const [parcelValue, setParcelValue] = useState("");
-
+  // Função para lidar com a mudança no valor a ser parcelado
   const handleValue = (e: ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     const formattedValue = formatCurrency({ value: inputValue });
     setValue(formattedValue);
   };
+  
+  // Função para lidar com a mudança no valor da entrada
   const handleDownValue = (e: ChangeEvent<HTMLInputElement>) => {
     const inputDownValue = e.target.value;
     const formattedDownValue = formatCurrency({ value: inputDownValue });
     setDownValue(formattedDownValue);
   };
 
-  useEffect(() => {
-    prevDownValue.current = downValue;
-    prevValue.current = value;
-    prevParcel.current = parcel;
-  }, [value, downValue, parcel]);
-
+  // Função para lidar com a mudança na quantidade de parcelas
   const handleParcel = (e: ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
     const numericValue = inputValue === "" ? "" : inputValue;
@@ -53,25 +48,34 @@ const Parcela = () => {
     }
   };
 
+  // Função para lidar com o cálculo das parcelas
   const handleCalculate = () => {
     if (value && parcel && downValue) {
       const calculatedParcel = parcelCalculator(value, parcel, downValue);
       setParcelValue(calculatedParcel);
-/*       setInformedValue(value);
-      setInformedParcel(parcel);
-      setInformedDownValue(downValue); */
+      
+      // Atualização dos valores anteriores após o cálculo
+      prevDownValue.current = downValue;
+      prevValue.current = value;
+      prevParcel.current = parcel;
+
+      // Limpeza dos campos após o cálculo
       setDownValue("");
       setValue("");
       setParcel("");
+    }
+    else {
+      alert("Preencha todos os campos");
     }
   }
 
   return (
     <div className="bg-zinc-900 min-h-screen flex flex-col">
-      <Header />
+      <Header /> {/* Componente de cabeçalho */}
       <div className="flex flex-grow justify-center items-center">
         <div className="bg-white rounded-lg shadow-5xl p-6 w-auto">
           <div className="flex flex-col space-y-4">
+            {/* Campos de entrada para valores */}
             <input
               type="text"
               placeholder="Valor da Entrada"
@@ -95,7 +99,9 @@ const Parcela = () => {
               onChange={handleParcel}
               value={parcel}
             />
+            {/* Botão para calcular as parcelas */}
             <button onClick={handleCalculate} className="bg-gradient-to-b from-yellow-500 to-yellow-600 text-lg p-4 rounded-md">Ver Valor da Parcela</button>
+            {/* Exibição dos valores anteriores e do valor de cada parcela */}
             {parcelValue && (
               <>
                 <h1>Valor de Entrada: <span className="text-yellow-500 font-bold">{prevDownValue.current}</span></h1>
